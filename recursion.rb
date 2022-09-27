5d11abcc34fe847ba668584db81c6f813d0ebdf3
tingamapuro04 <alponcemobutu100@gmail.com>
2022-09-28 01:10:05 +0300

    first commit

---
+++ recursion.rb
@@ -0,0 +1,31 @@
+def reverse_string(str)
+  return '' if str.length == 0
+  return reverser(str[1..-1]) + str[0]
+end
+
+def palindrome(str)
+  return true if str.length == 1 || str.length == 0
+  if (str[0] == str[-1])
+    pali(str[1..-2])
+  else
+    return false
+  end
+end
+
+def decimal_to_binary(num, tot = '')
+  if num < 1
+    return tot.reverse.to_i
+  else
+    bina(num/2, tot += (num%2).to_s)
+  end
+end
+
+def sum_natural_numbers(num)
+  return num if num == 0
+  return num + sum_natural_numbers(num-1)
+end
+
+def factorial(num)
+  return 1 if num == 1 || num == 0
+  return num * factorial(num-1)
+end
